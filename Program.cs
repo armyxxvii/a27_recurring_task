c35db76698e6a39b645d01139e1c17b9f2dd1eca
二七_HomePC <armyxxvii@gmail.com>
2025-08-03 06:27:26 +0800

    更新 wwwroot、嘗試修報錯

--- Program.cs
+++ Program.cs
@@ -4,4 +4,11 @@ var app = builder.Build();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
+// 添加 X-Content-Type-Options 標頭
+app.Use(async (context, next) =>
+{
+    context.Response.Headers.Append("X-Content-Type-Options", "nosniff");
+    await next();
+});
+
 app.Run();
